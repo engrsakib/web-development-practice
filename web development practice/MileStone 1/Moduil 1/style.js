e37119e7af8cf35df8f49688d4088f87b6963df0
engrsakib <snazmussakib01@gmail.com>
2024-07-02 02:11:41 +0600

    as

--- web development practice/MileStone 1/Moduil 1/style.js
+++ web development practice/MileStone 1/Moduil 1/style.js
@@ -2,3 +2,14 @@ console.log("Sakib khan");
 const a = 6,
   b = 9;
 console.log(a * b);
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+  var imageUrl = document.getElementById('imageToDownload').src;
+  var link = document.createElement('a');
+  link.href = imageUrl;
+  link.download = 'downloaded_image.jpg';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
